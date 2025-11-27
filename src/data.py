e7eb6097e7485d3952624fecadab8dc956930db0
yassine-turin <yassine@turintech.ai>
2025-11-27 12:37:15 +0000

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,1 +1,14 @@
+import json
+import random
+
+def load_ds1000(dataset_path: str, library: str = "pandas"):
+    """
+    Load a subset of DS-1000 filtered by library tag.
+    """
+    with open(dataset_path, "r") as f:
+        data = json.load(f)
+
+    tasks = [ex for ex in data if library.lower() in ex.get("libraries", [])]
+    random.shuffle(tasks)
+    return tasks
 
