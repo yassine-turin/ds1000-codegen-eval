f6509e5c297f105e93a566d157c210c6082b92b7
yassine-turin <yassine@turintech.ai>
2025-11-27 13:30:54 +0000

    Update run_eval.py

--- scripts/run_eval.py
+++ scripts/run_eval.py
@@ -1,7 +1,11 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 import argparse
-from src.config import Config
-from src.data import load_ds1000
-from src.eval import run_task
+from ds1000_eval.config import Config
+from ds1000_eval.data import load_ds1000
+from ds1000_eval.eval import run_task
 
 def main():
     parser = argparse.ArgumentParser()
